f24c3189fec187bf02a55096d9c83065483f00c6
Niklas Röttger <niklas.roettger@outlook.de>
2023-09-04 10:02:37 +0200

    add demo code

--- pages/movies/index.js
+++ pages/movies/index.js
@@ -1,13 +1,22 @@
 import Link from "next/link";
+import { movies } from "@/lib/data";
+import Head from "next/head";
 
 export default function Movies() {
   return (
     <>
+      <Head>
+        <title>List of Movies</title>
+      </Head>
       <h1>List of Movies</h1>
       <ul>
-        <li>
-          <Link href={`/movies/aquaman`}>Aquaman</Link>
-        </li>
+        {movies.map((movie) => {
+          return (
+            <li key={movie.id}>
+              <Link href={`/movies/${movie.slug}`}>{movie.title}</Link>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
